eefb42ee861d369fa66b33a9fa76ea0fc34fbb92
Blake Rego <brego@greenchargenet.com>
2013-01-30 16:12:18 -0500

    adding rails web server.

---
+++ webserver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webserver::Application.config.secret_token = '26461375b01a0e2a5650d0d1142307a49ae3d5739554ee701064d755c73b660a1c9747ac038c622590efdd3da62b5a4beb80f07523d10285bacdb4d67ac50b7a'
