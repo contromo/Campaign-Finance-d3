b86c602bebdc6b6701a1e6b1459b1935af3a9a5b
blake <blake.rego@gmail.com>
2013-02-02 09:04:48 -0500

    js tweak.

--- aggregatorService.js
+++ aggregatorService.js
@@ -8,9 +8,14 @@ function GetNewAggregator(url)
 		var a = []; //replace with actual call to web service. 
 
 		// say we have a function GetSum() that will sum up numbers 
-		// in an array. 
-		//return sum + GetSum(a);
+		// in an array.
+		return sum1 + GetSum(a);
 	}
+}
+
+function GetSum(a)
+{
+	return a + 5;
 }
 
 
