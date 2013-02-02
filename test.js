41b572780744c9fe112ae9b98a7327d6342f95d1
manav <mm2625@tc.columbia.edu>
2013-02-02 10:53:32 -0500

    Added multiple maps and legend

--- test.js
+++ test.js
@@ -2,20 +2,22 @@
 
 alert('new onyong');
 
-
+var d = null;
 function thisIsMyCallback(data) 
 {
 	alert('onyong from the right place');
+	d = data;
 	alert(data);
+	console.log(d);
 }
 
 var endpoint = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower";
 
 $('#getdata-button').live('click', function(){
-    $.getJSON(endpoint + '&callback=thisIsMyCallback()', function(data) {
+    $.getJSON(endpoint + '&callback=?', function(data) {
         //alert(data); //uncomment this for debug
         //alert (data.item1+" "+data.item2+" "+data.item3); //further debug
-        output = data.contributor_name;
+        var output = data.contributor_name;
 
     });
 });
