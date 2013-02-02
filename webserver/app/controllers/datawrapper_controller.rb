a72ad3a986fb9125bead4aab3dab64c4e9700a6c
blake <blake.rego@gmail.com>
2013-02-02 09:03:31 -0500

    parsing page number.

--- webserver/app/controllers/datawrapper_controller.rb
+++ webserver/app/controllers/datawrapper_controller.rb
@@ -3,21 +3,23 @@ class DatawrapperController < ApplicationController
 	# GET
 	def alldata
 		puts '################# onyong #####################'
+		page = params['page']
+		if !page.nil?
+			puts page
+		else
+			puts 'page is nil'
+		end
 		require 'net/http'
 
-		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower"
+		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower&per_page=100000"
 		uri = URI.parse(URI.encode(url.strip))
-		puts 'uri: ' + uri.to_s
-		puts 'uri.path: ' + uri.path
-		puts 'uri.port: ' + uri.port.to_s
-		puts 'uri.query: ' + uri.query
 
 		@result = Net::HTTP.get_response(uri)
 
-    respond_to do |format|
-      format.json { render json: @result.body }
-      format.js { render result.body }
-    end
+	    respond_to do |format|
+	      format.json { render json: @result.body }
+	      format.js { render @result.body }
+	    end
 
 	end
 end
