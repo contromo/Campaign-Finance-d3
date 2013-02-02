af775097b469560d1dc2fd0fb3e3972afb36e1de
Blake Rego <brego@greenchargenet.com>
2013-02-02 11:46:47 -0500

    adding ability to query different pages from the api.

--- webserver/app/controllers/datawrapper_controller.rb
+++ webserver/app/controllers/datawrapper_controller.rb
@@ -4,14 +4,16 @@ class DatawrapperController < ApplicationController
 	def alldata
 		puts '################# onyong #####################'
 		page = params['page']
+
 		if !page.nil?
-			puts page
+			puts 'page is ' + page
 		else
-			puts 'page is nil'
+			page = 1
 		end
+		
 		require 'net/http'
 
-		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower&per_page=100000"
+		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower&per_page=100000&page=" + page
 		uri = URI.parse(URI.encode(url.strip))
 
 		@result = Net::HTTP.get_response(uri)
@@ -23,3 +25,5 @@ class DatawrapperController < ApplicationController
 
 	end
 end
+
+
