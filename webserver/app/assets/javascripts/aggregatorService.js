1a6ec40e9c24f8fc9e092e79930e2951eeece8b8
Charles <csm2115@gmail.com>
2013-02-02 18:48:00 -0500

    Blake/Manav found an issue where there were no democrat funding from google. Bug was a misspelling of recipient_party

--- webserver/app/assets/javascripts/aggregatorService.js
+++ webserver/app/assets/javascripts/aggregatorService.js
@@ -1,5 +1,196 @@
 
-function calcSumPerDistrict(pSeatType, pJsonData)
+function calcSumPerDistrict(pJsonData)
+{
+	//Example data. This should be discarded once the pJsonData variable is fed into this function
+
+	var result_json = [];
+	var mDict = {};
+
+	$.each(pJsonData, function(key, data){
+	if(data.seat == "federal:house")
+	{
+		if(data.district == null || data.district.toString() == "")
+		{
+			console.log("Iteration: " + key.toString() + " has a blank/null district");
+		}
+		else
+		{
+			var district = data.district.toString().replace("-","_");
+			var amount_obj = data.amount;
+
+			var temp = mDict[district];
+
+			if(typeof(temp) != 'undefined')
+			{
+				if(data.recipient_party == "R")
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.repub_don, 10);
+					mDict[district].repub_don = temp_amount;
+				}
+				else if(data.recipient_part == "D")
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.dem_don, 10);
+					mDict[district].dem_don = temp_amount;
+				}
+				else
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.other_don, 10);
+					mDict[district].other_don = temp_amount;
+				}
+			}
+			else
+			{
+				if(data.recipient_party == "R")
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = amount_obj.toString();
+					json.dem_don = 0;
+					json.other_don = 0;
+
+					mDict[district] = json;
+				}
+				else if(data.recipient_part == "D")
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = 0;
+					json.dem_don = amount_obj.toString();
+					json.other_don = 0;
+
+					mDict[district] = json;
+				}
+				else
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = 0;
+					json.dem_don = 0;
+					json.other_don = amount_obj.toString();
+					mDict[district] = json;
+				}
+			}
+		}
+	}
+
+	});
+
+	for(var p in mDict)
+	{
+		result_json.push(mDict[p]);
+	}
+
+	return result_json;
+
+	// The inclusion of this function is only for debugging purposes.
+	//DumpDict(mDict);
+
+}
+
+
+function calcSumPerState(pJsonData)
+{
+	//Example data. This should be discarded once the pJsonData variable is fed into this function
+
+	var result_json = [];
+	var mDict = {};
+
+	$.each(pJsonData, function(key, data){
+	if(data.seat == "federal:senate")
+	{
+		if(data.recipient_state == null || data.district.toString() == "")
+		{
+			console.log("Iteration: " + key.toString() + " has a blank/null district");
+		}
+		else
+		{
+			var district = data.district.toString().replace("-","_");
+			var amount_obj = data.amount;
+
+			var temp = mDict[district];
+
+			if(typeof(temp) != 'undefined')
+			{
+				if(data.recipient_party == "R")
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.repub_don, 10);
+					mDict[district].repub_don = temp_amount;
+				}
+				else if(data.recipient_part == "D")
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.dem_don, 10);
+					mDict[district].dem_don = temp_amount;
+				}
+				else
+				{
+					var temp_amount = parseInt(amount_obj, 10) + parseInt(temp.other_don, 10);
+					mDict[district].other_don = temp_amount;
+				}
+			}
+			else
+			{
+				if(data.recipient_party == "R")
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = amount_obj.toString();
+					json.dem_don = 0;
+					json.other_don = 0;
+
+					mDict[district] = json;
+				}
+				else if(data.recipient_part == "D")
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = 0;
+					json.dem_don = amount_obj.toString();
+					json.other_don = 0;
+
+					mDict[district] = json;
+				}
+				else
+				{
+					var json = {};
+					json.district = district;
+					json.repub_don = 0;
+					json.dem_don = 0;
+					json.other_don = amount_obj.toString();
+					mDict[district] = json;
+				}
+			}
+		}
+	}
+
+	});
+
+	for(var p in mDict)
+	{
+		result_json.push(mDict[p]);
+	}
+
+	return result_json;
+
+	// The inclusion of this function is only for debugging purposes.
+	//DumpDict(mDict);
+
+}
+
+
+
+function DumpDict(pDict)
+{
+	var s = 0;
+	for(var p in pDict)
+	{
+		//alert(pDict[p].district + "," + pDict[p].district.repub_don + "," + pDict[p].district.dem_don + "," + pDict[p].district.other_don);
+		alert(JSON.stringify(pDict[p]));
+		s++;
+	}
+	alert(s);
+}
+
+function calcMaxPerDistrict(pSeatType, pJsonData)
 {
 //Example data. This should be discarded once the pJsonData variable is fed into this function
 
@@ -85,17 +276,4 @@ return result_json;
 // The inclusion of this function is only for debugging purposes.
 //DumpDict(mDict);
 
-}
-
-
-function DumpDict(pDict)
-{
-	var s = 0;
-	for(var p in pDict)
-	{
-		//alert(pDict[p].district + "," + pDict[p].district.repub_don + "," + pDict[p].district.dem_don + "," + pDict[p].district.other_don);
-		alert(JSON.stringify(pDict[p]));
-		s++;
-	}
-	alert(s);
 }
