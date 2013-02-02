9efd925d31eb7493acb45b630c55f1488afd6169
Blake Rego <brego@greenchargenet.com>
2013-02-01 19:51:57 -0500

    adding transparency_data gem. currently this causes my computer to not start the server properly because curllib is not working. going to try this in the mac environment (but starting a separate branch to be safe).

--- webserver/app/controllers/datawrapper_controller.rb
+++ webserver/app/controllers/datawrapper_controller.rb
@@ -4,6 +4,13 @@ class DatawrapperController < ApplicationController
 	def alldata
 		puts '################# onyong #####################'
 		require 'net/http'
+		require 'transparency_data'
+
+		# contributions with an amount greater than or equal to $1000
+		puts 'using the api: ' + TransparencyData::Client.contributions(:contributor_ft => 'steve jobs', :amount => {:gte => 1000})
+
+
+
 
 		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower"
 		uri = URI.parse(URI.encode(url.strip))
---
+++ webserver/config/initializers/transparency_data.rb
@@ -0,0 +1,5 @@
+
+
+TransparencyData.configure do |config|
+ config.api_key = 'ac889422c02d446383ac5fffcebf5665'
+end
