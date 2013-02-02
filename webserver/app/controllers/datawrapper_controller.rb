ac0eb36066abfb819e190c9803d19e0a3f630d6e
Blake Rego <brego@greenchargenet.com>
2013-02-02 12:59:07 -0500

    adding endpoint to get all companies.

--- webserver/app/controllers/datawrapper_controller.rb
+++ webserver/app/controllers/datawrapper_controller.rb
@@ -1,29 +1,46 @@
 class DatawrapperController < ApplicationController
-	
+
+
+
 	# GET
 	def alldata
-		puts '################# onyong #####################'
-		page = params['page']
+		apikey = 	"ac889422c02d446383ac5fffcebf5665"
 
+		puts '################# alldata #####################'
+		page = parsePage
+		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=" + apikey + "&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower&per_page=100&page=" + page		
+		query_api(url, page)
+	end
+
+
+	# GET
+	def companynames
+			apikey = 	"ac889422c02d446383ac5fffcebf5665"
+			puts '################# companynames #####################'
+			page = parsePage
+			url = "http://transparencydata.com/api/1.0/entities/list.json?apikey=" + apikey + "&start=0&end=100&type=organization&page=" + page		
+			query_api(url, page)
+	end
+
+
+	def parsePage
+		page = params['page']
 		if !page.nil?
 			puts 'page is ' + page
 		else
 			page = 1
-		end
-		
-		require 'net/http'
+		end	
+		return page
+	end
 
-		url = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower&per_page=100000&page=" + page
-		uri = URI.parse(URI.encode(url.strip))
-
-		@result = Net::HTTP.get_response(uri)
-
+	def query_api(url, page)
+			require 'net/http'
+			uri = URI.parse(URI.encode(url.strip))
+			@result = Net::HTTP.get_response(uri)
 	    respond_to do |format|
 	      format.json { render json: @result.body }
 	      format.js { render @result.body }
 	    end
-
 	end
-end
 
-
+end #ends class
--- webserver/config/routes.rb
+++ webserver/config/routes.rb
@@ -2,4 +2,5 @@ Webserver::Application.routes.draw do
   root :to => 'static#start'
   match ':action' => 'static#:action'
   match 'datawrapper/alldata' => 'datawrapper#alldata'
+  match 'datawrapper/companynames' => 'datawrapper#companynames'  
 end
