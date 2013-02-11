48aca5bae7ea4380ad684d80c7cf93a1c1a061f1
Charles <csm2115@gmail.com>
2013-02-10 21:19:00 -0500

    Replaced contents of the javascript dir from local copy of previous revision

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,11 @@
+
+
+var blah; 
+
+function thisIsMyCallback(data) 
+{
+	blah = data;
+	alert('done');
+}
+
+var endpoint = "http://transparencydata.com/api/1.0/contributions.json?apikey=ac889422c02d446383ac5fffcebf5665&contributor_state=ak&date=><|2002-09-01|2002-12-31&seat=state:upper|state:lower";
